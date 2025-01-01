c2eb203bc1805e6fa8cf6799d254ebc2bfbd5da0
Mohamed-bek <mohamedbekkouche0302@gmail.com>
2025-01-01 22:25:22 +0100

    v1

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ import dotenv from "dotenv";
 import transactionRouter from "./routes/transactions.js";
 import userRouter from "./routes/users.js";
 import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url"; // For ES modules, use fileURLToPath
+import path from "path"; // Import path
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -26,6 +31,16 @@ mongoose
 
 app.use("/transaction", transactionRouter);
 app.use("/user", userRouter);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from React app
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  // Serve index.html for all other requests
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
